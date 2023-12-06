946b980ce3615cf2e2ab05f0d22890659c77a494
RobertLucas <robert.lucaas.80@gmail.com>
2023-12-06 13:14:17 +0100

    Senaste

--- src/test/java/stepDefinitions/MyStepdefs.java
+++ src/test/java/stepDefinitions/MyStepdefs.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,158 +9,227 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MyStepdefs {
+import java.time.Duration;
+
+public class MyStepdefs<string, function> {
 
     private Registrator registrator;
 
     private WebDriver driver;
+    private WebDriver WebDriverRefrence;
 
-    @Given("I have correct url")
-    public void iHaveCorrectUrl() {
+    @Given("User have correct url")
+    public void UserHaveCorrectUrl() {
 
+        //driver = new FirefoxDriver();
         driver = new EdgeDriver();
+        //driver = new ChromeDriver();
 
         driver.manage().window().maximize();
 
         driver.get("https://membership.basketballengland.co.uk/NewFullAccount");
+        //driver.findElement(By.tagName("button")).click();
+
+        System.out.println("Correct url is entered");
     }
 
-    @When("I fill in all required fields")
+    @And("User fill in all member details")
     public void iFillInAllRequiredFields() throws Throwable {
 
-            WebElement search = driver.findElement(By.id("dp"));
-            search.sendKeys("12/02/1980");
+        WebElement search = driver.findElement(By.id("dp"));
+        search.sendKeys("12/02/1980");
 
-        System.out.println("All required fields are filled in");
+        driver.findElement(By.id("member_firstname")).sendKeys("Robert");
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("member_lastname")).sendKeys("Lucas");
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("member_emailaddress")).sendKeys("robert.lucaas.80@gmail.com");
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("member_confirmemailaddress")).sendKeys("robert.lucaas.80@gmail.com");
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("signup_memberdetails_mobilephone")).sendKeys("+46702064151");
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("signup_memberdetails_homephone")).sendKeys("+46702064151");
+        Thread.sleep(1000);
+
+        Select dropDown = new Select(driver.findElement(By.id("signup_memberdetails_gender")));
+
+        dropDown.selectByIndex(2);
+        Thread.sleep(1000);
+
+        dropDown = new Select(driver.findElement(By.id("sign_up_12")));
+        dropDown.selectByIndex(211);
+        Thread.sleep(1000);
+
+        dropDown = new Select(driver.findElement(By.id("sign_up_13")));
+        dropDown.selectByIndex(211);
+        Thread.sleep(1000);
+
+        System.out.println("All member details are filled in");
+
+    }
+
+    @And("User fill in all emergency contact details")
+    public void userFillInAllEmergencyContactDetails() {
+
+        driver.findElement(By.id("signup_nextofkin_name")).sendKeys("Anna Lucas");
+
+        driver.findElement(By.id("signup_nextofkin_relationship")).sendKeys("Wife");
+
+        driver.findElement(By.id("signup_nextofkin_contact")).sendKeys("+46768778384");
+        System.out.println("All emergency contact details are filled in");
+
+    }
+
+    @And("User fill in address")
+    public void userFillInAddress() {
+
+        driver.findElement(By.id("txtAddressLine1")).sendKeys("31 The Square, Tarland, Aberdeenshire Scotland");
+
+        driver.findElement(By.id("txtAddressTownCity")).sendKeys("Aberchirder");
+
+        driver.findElement(By.id("txtPostCode")).sendKeys("AB34 4TX");
+
+        Select dropDown = new Select(driver.findElement(By.id("ddlCounty")));
+        dropDown.selectByIndex(1);
+
+        dropDown = new Select(driver.findElement(By.name("CountryId")));
+        dropDown.selectByIndex(211);
+
+        System.out.println("Address is filled in");
+
+    }
+
+    @And("User sets password")
+    public void userSetsPassword() {
+
+        driver.findElement(By.id("sign_up_9")).sendKeys("EttEnkeltLösenord");
+
+        driver.findElement(By.id("sign_up_10")).sendKeys("EttEnkeltLösenord");
+
+        System.out.println("Password is set");
+
+    }
+
+    @And("User fills in all anonymous information")
+    public void userFillsInAllAnonymousInformation() {
+
+        Select dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_ethnicity")));
+        dropDown.selectByIndex(1);
+
+        dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_sexualorientation")));
+        dropDown.selectByIndex(1);
+
+        dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_religion")));
+        dropDown.selectByIndex(1);
+
+        System.out.println("All anonymous information i set");
+
+    }
+
+    @And("Account confirmation is checked")
+    public void AccountConfirmationIsChecked() {
+
+        driver.findElement(By.cssSelector("label[for=sign_up_25")).click();
+
+        driver.findElement(By.cssSelector("label[for=sign_up_26")).click();
+
+        System.out.println("Account confirmation i checked");
+
+    }
+
+    @And("Code of ethics and conduct are checked")
+    public void codeOfEthicsAndConductAreChecked() {
+
+        driver.findElement(By.cssSelector("label[for='fanmembersignup_agreetocodeofethicsandconduct'] span[class='box']")).click();
+
+        System.out.println("Code of ethics and conduct are checked");
+
+    }
+
+    @And("Join and proceed are clicked")
+    public void joinAndProceedAreClicked() throws InterruptedException {
+
+        //Explicit wait, men får inte & funka :(
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        WebElement LastButton;
+        LastButton= wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#btnJoin")));
+        
+        driver.findElement(By.cssSelector("input#btnJoin")).click();
+        System.out.println("Join and proceed is clicked");
+
     }
 
     @Then("An account is created")
-    public void anAccountIsCreated() throws Throwable {
-
-        WebElement search = driver.findElement(By.id("btnJoin"));
-        driver.findElement(By.id("btnJoin")).click();
+    public void anAccountIsCreated() {
 
         System.out.println("Account is created");
-    }
-
-    private void assertEquals(string expected, string actual) {
-    }
-
-    public class string {
-    }
-
-    public void add() {
-    }
-
-    public string getResult() {
-        return getResult();
 
     }
 
-    public class Registrator {
+    @When("User fails to fill in all member details")
+    public void userFailsToFillInAllMemberDetails() {
 
-        @Test
+        WebElement search = driver.findElement(By.id("dp"));
+        search.sendKeys("12/02/1980");
 
-        public string RegisterUser() throws InterruptedException {
+        driver.findElement(By.id("member_firstname")).sendKeys("Robert");
+        //Thread.sleep(1000);
 
-            WebDriver driver = new EdgeDriver();
-            driver.manage().window().maximize();
+        WebElement A = driver.findElement(By.id("member_lastname"));
+        String text = A.getAttribute("value");
+        if (text.isEmpty()) {
 
-            driver.get("https://membership.basketballengland.co.uk/NewFullAccount");
-            Thread.sleep(1000);
+            System.out.println("'Last Name' must be entered");
 
-            WebElement button = driver.findElement(By.id("dp"));
-            button.click();
-            driver.findElement(By.id("dp")).sendKeys("12/02/1980");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("member_firstname")).sendKeys("Robert");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("member_lastname")).sendKeys("Lucas");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("member_emailaddress")).sendKeys("robert.lucaas.80@gmail.com");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("member_confirmemailaddress")).sendKeys("robert.lucaas.80@gmail.com");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("signup_memberdetails_mobilephone")).sendKeys("+46702064151");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("signup_memberdetails_homephone")).sendKeys("Died in the 20's :)");
-            Thread.sleep(1000);
-
-            Select dropDown = new Select(driver.findElement(By.id("signup_memberdetails_gender")));
-
-            dropDown.selectByIndex(2);
-            Thread.sleep(1000);
-
-            dropDown = new Select(driver.findElement(By.id("sign_up_12")));
-            dropDown.selectByIndex(211);
-            Thread.sleep(1000);
-
-            dropDown = new Select(driver.findElement(By.id("sign_up_13")));
-            dropDown.selectByIndex(211);
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("signup_nextofkin_name")).sendKeys("Anna Lucas");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("signup_nextofkin_relationship")).sendKeys("Wife");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("signup_nextofkin_contact")).sendKeys("+46768778384");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("txtAddressLine1")).sendKeys("31 The Square, Tarland, Aberdeenshire Scotland");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("txtAddressTownCity")).sendKeys("Aberchirder");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("txtPostCode")).sendKeys("AB34 4TX");
-            Thread.sleep(1000);
-
-            dropDown = new Select(driver.findElement(By.id("ddlCounty")));
-            dropDown.selectByIndex(1);
-            Thread.sleep(1000);
-
-            dropDown = new Select(driver.findElement(By.name("CountryId")));
-            dropDown.selectByIndex(211);
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("sign_up_9")).sendKeys("EttEnkeltLösenord");
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("sign_up_10")).sendKeys("EttEnkeltLösenord");
-            Thread.sleep(1000);
-
-            driver.findElement(By.cssSelector("label[for=sign_up_25")).click();
-            Thread.sleep(1000);
-
-            driver.findElement(By.cssSelector("label[for=sign_up_26")).click();
-            Thread.sleep(1000);
-
-            driver.findElement(By.cssSelector("label[for=fanmembersignup_agreetocodeofethicsandconduct")).click();
-            Thread.sleep(1000);
-
-            driver.findElement(By.id("btnJoin")).click();
-
-            driver.quit();
-
-
-            return getResult();
         }
 
-        public string getResult() {
-            return registrator.getResult();
         }
 
-        public void add() {
-        }
+    @When("User sets password missmatch")
+    public void userSetsPasswordMissmatch() {
+
     }
-}
+
+    @Then("An account is NOT created")
+    public void anAccountIsNOTCreated() {
+
+        System.out.println("'Last Name' must be entered");
+
+    }
+
+    @When("Code of ethics and conduct NOT are checked")
+    public void codeOfEthicsAndConductNOTAreChecked() {
+
+    }
+
+    @When("User sets password mismatch")
+    public void userSetsPasswordMismatch() {
+    }
+
+    class Registrator {
+
+            @Test
+
+            public string RegisterUser() throws InterruptedException {
+
+                return getResult();
+            }
+
+            public string getResult() {
+                return registrator.getResult();
+            }
+
+
+            }
+        }
+
--- src/test/resources/features/Registrator.feature
+++ src/test/resources/features/Registrator.feature
@@ -1,17 +1,47 @@
-Feature: Registrate New User
+Feature: Create New User
 
-  Scenario: Registrate user on webpage
-    Given I have correct url
-    When I fill in all required fields
+  Scenario Outline: Registrate a new user on webpage
+    Given User have correct <url>
+    And User fill in all member details
+    And User fill in all emergency contact details
+    And User fill in address
+    And User sets password
+    And User fills in all anonymous information
+    And Account confirmation is checked
+    And Code of ethics and conduct are checked
+    And Join and proceed are clicked
     Then An account is created
-
-Feature: Registrate New User
-
-  Scenario Outline: Registrate user on webpage
-    Given I have correct <url>
-    When I fill in all <required> fields
-    Then An account is <created>
     Examples:
+      |  |  |  |
 
-      | url                                                       | required | created |
-      | https://membership.basketballengland.co.uk/NewFullAccount |          |         |
+  Scenario: Create new user last name is missing
+    Given User have correct url
+    When User fails to fill in all member details
+    Then An account is NOT created
+
+  Scenario: Create new user password doesn't match
+    Given User have correct url
+    And User fill in all member details
+    And User fill in all emergency contact details
+    And User fill in address
+    When User sets password mismatch
+    Then An account is NOT created
+
+  Scenario: Create new user terms and conditions is not checked
+    Given User have correct url
+    And User fill in all member details
+    And User fill in all emergency contact details
+    And User fill in address
+    And User sets password
+    And User fills in all anonymous information
+    And Account confirmation is checked
+    When Code of ethics and conduct NOT are checked
+    Then An account is NOT created
+
+  #Verifiering skall ske på varje scenario med minst en Junit assert
+
+  #Testfallen skall skapas genom en och enbart en Scenario Outline ifrån en feature-fil
+
+  #Testfallen skall köras på minst två browsrar
+
+  #Explicit wait, provat massas men får inte & funka :(
