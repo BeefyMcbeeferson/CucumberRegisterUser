cb75e18699a222c2bbe7e3620cabb12286dc4b91
RobertLucas <robert.lucaas.80@gmail.com>
2023-12-11 14:57:55 +0100

    Latest

--- src/test/java/stepDefinitions/MyStepdefs.java
+++ src/test/java/stepDefinitions/MyStepdefs.java
@@ -7,7 +7,6 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -16,123 +15,13 @@ import org.testng.Assert;
 
 import java.time.Duration;
 
-public class MyStepdefs<string, function> {
+public class MyStepdefs {
 
     private Registrator registrator;
 
     private WebDriver driver;
     private WebDriver WebDriverReference;
     private String browser;
-
-    @And("User fill in all member details")
-    public void iFillInAllRequiredFields() throws Throwable {
-
-        WebElement search = driver.findElement(By.id("dp"));
-        search.sendKeys("12/02/1980");
-
-        driver.findElement(By.id("member_firstname")).sendKeys("Robert");
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("member_lastname")).sendKeys("Lucas");
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("member_emailaddress")).sendKeys("robert.lucaas.80@gmail.com");
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("member_confirmemailaddress")).sendKeys("robert.lucaas.80@gmail.com");
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("signup_memberdetails_mobilephone")).sendKeys("+46702064151");
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("signup_memberdetails_homephone")).sendKeys("+46702064151");
-        Thread.sleep(1000);
-
-        Select dropDown = new Select(driver.findElement(By.id("signup_memberdetails_gender")));
-
-        dropDown.selectByIndex(2);
-        Thread.sleep(1000);
-
-        dropDown = new Select(driver.findElement(By.id("sign_up_12")));
-        dropDown.selectByIndex(211);
-        Thread.sleep(1000);
-
-        dropDown = new Select(driver.findElement(By.id("sign_up_13")));
-        dropDown.selectByIndex(211);
-        Thread.sleep(1000);
-
-        System.out.println("All member details are filled in");
-
-    }
-
-    @And("User fill in all emergency contact details")
-    public void userFillInAllEmergencyContactDetails() {
-
-        driver.findElement(By.id("signup_nextofkin_name")).sendKeys("Anna Lucas");
-
-        driver.findElement(By.id("signup_nextofkin_relationship")).sendKeys("Wife");
-
-        driver.findElement(By.id("signup_nextofkin_contact")).sendKeys("+46768778384");
-        System.out.println("All emergency contact details are filled in");
-
-    }
-
-    @And("User fill in address")
-    public void userFillInAddress() {
-
-        driver.findElement(By.id("txtAddressLine1")).sendKeys("31 The Square, Tarland, Aberdeenshire Scotland");
-
-        driver.findElement(By.id("txtAddressTownCity")).sendKeys("Aberchirder");
-
-        driver.findElement(By.id("txtPostCode")).sendKeys("AB34 4TX");
-
-        Select dropDown = new Select(driver.findElement(By.id("ddlCounty")));
-        dropDown.selectByIndex(1);
-
-        dropDown = new Select(driver.findElement(By.name("CountryId")));
-        dropDown.selectByIndex(211);
-
-        System.out.println("Address is filled in");
-
-    }
-
-    @And("User sets password")
-    public void userSetsPassword() {
-
-        driver.findElement(By.id("sign_up_9")).sendKeys("EttEnkeltLösenord");
-
-        driver.findElement(By.id("sign_up_10")).sendKeys("EttEnkeltLösenord");
-
-        System.out.println("Password is set");
-
-    }
-
-    @And("User fills in all anonymous information")
-    public void userFillsInAllAnonymousInformation() {
-
-        Select dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_ethnicity")));
-        dropDown.selectByIndex(1);
-
-        dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_sexualorientation")));
-        dropDown.selectByIndex(1);
-
-        dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_religion")));
-        dropDown.selectByIndex(1);
-
-        System.out.println("All anonymous information i set");
-
-    }
-
-    @And("Account confirmation is checked")
-    public void AccountConfirmationIsChecked() {
-
-        driver.findElement(By.cssSelector("label[for=sign_up_25")).click();
-
-        driver.findElement(By.cssSelector("label[for=sign_up_26")).click();
-
-        System.out.println("Account confirmation i checked");
-
-    }
 
     @And("Code of ethics and conduct are checked")
     public void codeOfEthicsAndConductAreChecked() {
@@ -156,47 +45,6 @@ public class MyStepdefs<string, function> {
 
     }
 
-    @Then("An account is created")
-    public void anAccountIsCreated() {
-
-        System.out.println("Account is created");
-
-    }
-
-    @When("User fails to fill in all member details")
-    public void userFailsToFillInAllMemberDetails() {
-
-        WebElement search = driver.findElement(By.id("dp"));
-        search.sendKeys("12/02/1980");
-
-        driver.findElement(By.id("member_firstname")).sendKeys("Robert");
-
-        WebElement A = driver.findElement(By.id("member_lastname"));
-        String text = A.getAttribute("value");
-        if (text.isEmpty()) {
-
-            System.out.println("'Last Name' must be entered");
-
-        }
-
-    }
-
-    @Then("An account is NOT created")
-    public void anAccountIsNOTCreated() {
-
-        System.out.println("'Last Name' must be entered");
-
-    }
-
-    @When("Code of ethics and conduct NOT are checked")
-    public void codeOfEthicsAndConductNOTAreChecked() {
-
-    }
-
-    @When("User sets password mismatch")
-    public void userSetsPasswordMismatch() {
-    }
-
     @When("User is on the registration page")
     public void userIsOnTheRegistrationPage() {
         String pageTitle = driver.getTitle();
@@ -204,33 +52,17 @@ public class MyStepdefs<string, function> {
 
     }
 
-    @And("User enter {string} and {string}")
-    public void enterUserAnd(String firstName, String lastName) {
-
-        driver.findElement(By.id("member_firstname")).sendKeys(firstName);
-        driver.findElement(By.id("member_lastname")).sendKeys(lastName);
-    }
-
-    @And("User enter email {string} and {string}")
-    public void userEnterEmailAnd(String Email, String confirmEmail) {
-
-        driver.findElement(By.id("member_firstname")).sendKeys(Email);
-        driver.findElement(By.id("member_firstname")).sendKeys(confirmEmail);
-
-    }
-
     @And("User fill in {string}")
     public void userFillIn(String Mobilephone) {
 
-        driver.findElement(By.id("signup_memberdetails_mobilephone")).sendKeys("+46702064151");
+        driver.findElement(By.id("signup_memberdetails_mobilephone")).sendKeys(Mobilephone);
 
     }
 
     @And("User selects {string}")
-    public void userSelects(String female, String male) {
+    public void userSelects(String male) {
 
         Select dropDown = new Select(driver.findElement(By.id("signup_memberdetails_gender")));
-
         dropDown.selectByIndex(2);
 
     }
@@ -247,31 +79,31 @@ public class MyStepdefs<string, function> {
     }
 
     @And("User enter emergency contact details {string} and {string} and {string}")
-    public void userEnterEmergencyContactDetailsAndAnd(String fullname, String relationship, String arg2, String phonenumber) {
+    public void userEnterEmergencyContactDetailsAndAnd(String fullname, String relationship, String phonenumber) {
 
-        driver.findElement(By.id("signup_nextofkin_name")).sendKeys("Anna Lucas");
+        driver.findElement(By.id("signup_nextofkin_name")).sendKeys(fullname);
 
-        driver.findElement(By.id("signup_nextofkin_relationship")).sendKeys("Wife");
+        driver.findElement(By.id("signup_nextofkin_relationship")).sendKeys(relationship);
 
-        driver.findElement(By.id("signup_nextofkin_contact")).sendKeys("+46768778384");
+        driver.findElement(By.id("signup_nextofkin_contact")).sendKeys(phonenumber);
 
         System.out.println("All emergency contact details are filled in");
 
     }
 
     @And("User fills in address {string} and {string} and {string} and {string} and {string}")
-    public void userFillsInAddressAndAndAndAnd(String addressline1, String towncity, String postcode, String country, String country2) {
+    public void userFillsInAddressAndAndAndAnd(String addressline1, String towncity, String postcode, String county, String country) {
 
-        driver.findElement(By.id("txtAddressLine1")).sendKeys("31 The Square, Tarland, Aberdeenshire Scotland");
+        driver.findElement(By.id("txtAddressLine1")).sendKeys(addressline1);
 
-        driver.findElement(By.id("txtAddressTownCity")).sendKeys("Aberchirder");
+        driver.findElement(By.id("txtAddressTownCity")).sendKeys(towncity);
 
-        driver.findElement(By.id("txtPostCode")).sendKeys("AB34 4TX");
+        driver.findElement(By.id("txtPostCode")).sendKeys(postcode);
 
         Select dropDown = new Select(driver.findElement(By.id("ddlCounty")));
         dropDown.selectByIndex(1);
 
-        dropDown = new Select(driver.findElement(By.name("CountryId")));
+        dropDown = new Select(driver.findElement(By.id("ddlCountry")));
         dropDown.selectByIndex(211);
 
         System.out.println("Address is filled in");
@@ -281,16 +113,16 @@ public class MyStepdefs<string, function> {
     @And("User sets {string} and {string}")
     public void userSetsAnd(String password, String retypepassword) {
 
-        driver.findElement(By.id("sign_up_9")).sendKeys("EttEnkeltLösenord");
+        driver.findElement(By.id("sign_up_9")).sendKeys(password);
 
-        driver.findElement(By.id("sign_up_10")).sendKeys("EttEnkeltLösenord");
+        driver.findElement(By.id("sign_up_10")).sendKeys(retypepassword);
 
         System.out.println("Password is set");
 
     }
 
     @And("User fills in all anonymous information {string} and {string} and {string}")
-    public void userFillsInAllAnonymousInformationAndAnd(String sexualorientation, String religion) {
+    public void userFillsInAllAnonymousInformationAndAnd(String ethnicity, String sexualorientation, String religion) {
 
         Select dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_ethnicity")));
         dropDown.selectByIndex(1);
@@ -319,7 +151,7 @@ public class MyStepdefs<string, function> {
     @And("{string} are checked")
     public void areChecked(String codeofethicsandconduct) {
 
-        driver.findElement(By.cssSelector("label[for='fanmembersignup_agreetocodeofethicsandconduct'] span[class='box']")).click();
+        driver.findElement(By.cssSelector("div:nth-of-type(7) > label > .box)).")).click();
 
         System.out.println("Code of ethics and conduct are checked");
 
@@ -329,9 +161,9 @@ public class MyStepdefs<string, function> {
     public void areClicked(String Joinandproceed) {
 
         //Explicit wait, men får inte & funka :(
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        WebElement LastButton;
-        LastButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#btnJoin")));
+        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        //WebElement LastButton;
+        //LastButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(Joinandproceed)));
 
         driver.findElement(By.cssSelector("input#btnJoin")).click();
         System.out.println("Join and proceed is clicked");
@@ -345,17 +177,32 @@ public class MyStepdefs<string, function> {
 
     }
 
-    @Given("User has the correct {string}")
-    public void userHasTheCorrect(String url) {
+    @And("User enter {string} {string} and {string}")
+    public void userEnterAnd(String DateOfBirth, String firstName, String lastName) {
+
+            driver.findElement(By.id("dp")).sendKeys(DateOfBirth);
+            driver.findElement(By.id("member_firstname")).sendKeys(firstName);
+            driver.findElement(By.id("member_lastname")).sendKeys(lastName);
+        }
+
+    @Given("User is on the correct {string}")
+    public void userIsOnTheCorrect(String url) {
 
         driver = new EdgeDriver();
         driver.manage().window().maximize();
-        driver.navigate().to("https://membership.basketballengland.co.uk/NewFullAccount");
         driver.get(url);
-
     }
 
-    @Given("User is using {string} as {string}")
+    @And("User enter email {string} and {string}")
+    public void userEnterEmailAnd(String Email, String confirmEmail) {
+
+        driver.findElement(By.id("member_emailaddress")).sendKeys(Email);
+        driver.findElement(By.id("member_confirmemailaddress")).sendKeys(confirmEmail);
+
+    }
+}
+
+    /*@Given("User is using {string} as {string}")
     public void userIsUsingAs(String arg0, String arg1) {
 
         {
@@ -364,30 +211,15 @@ public class MyStepdefs<string, function> {
             } else if (browser.equals("chrome")) {
                 driver = new ChromeDriver();
             }
+            driver.get("https://membership.basketballengland.co.uk/NewFullAccount");
 
-        }
-    }
+        }*/
+
 
     class Registrator {
 
-
-        /*@Given("User is using {string} as {string}")
-        public void userIsUsingAs(String edge, String chrome) {
-
-            {
-                if (browser.equals(("edge"))) {
-                    driver = new EdgeDriver();
-                } else if (browser.equals("chrome")) {
-                    driver = new ChromeDriver();
-                }
-
-                driver.get("https://membership.basketballengland.co.uk/NewFullAccount");
-
-            }*/
-
-
     }
-}
+
 
 
 
--- src/test/resources/features/Registrator.feature
+++ src/test/resources/features/Registrator.feature
@@ -1,27 +1,24 @@
 Feature: Create New User
 
   Scenario Outline: Registration a new user on webpage
-    Given User has the correct "<url>"
-    Given User is using "edge" as "<browser>"
-    When User is on the registration page
-    And User enter "<First Name>" and "<Last Name>"
-    And User enter email "<Email>" and "<Email>"
-    And User fill in "<Mobile phone>"
+    #Given User is using "edge" as "<browser>"
+    Given User is on the correct "<url>"
+    And User enter "<DateOfBirth>" "<First Name>" and "<Last Name>"
+    And User enter email "<Email>" and "<confirmEmail>"
+    And User fill in "<Mobilephone>"
     And User selects "<gender>"
     And User enters "<nationality>" and "<country of birth>"
-    And User enter emergency contact details "<full name>" and "<relationship>" and "<phone number>"
-    And User fills in address "<address line 1>" and "<town/city>" and "<post code>" and "<country>" and "<country 2>"
-    And User sets "<password>" and "<retype password>"
+    And User enter emergency contact details "<full name>" and "<relationship>" and "<phonenumber>"
+    And User fills in address "<address line 1>" and "<towncity>" and "<post code>" and "<county>" and "<country>"
+    And User sets "<password>" and "<retypepassword>"
     And User fills in all anonymous information "<ethnicity>" and "<sexual orientation>" and "<religion>"
     And "<Account confirmation>" is checked
-    And "<Code of ethics and conduct>" are checked
+    And "<Code of ethics and conduct>" are clicked
     And "<Join and proceed>" are clicked
     Then An "<Account>" is created
     Examples:
-      | url                                                       | browser | First Name | Last Name | Email                      | Mobile phone | gender | nationality | country of birth | full name | relationship | phone number | address line 1                                 | town/city   | post code | country       | country 2 | password          | retype password   | ethnicity         | sexual orientation | religion         | Account confirmation |Code of ethics and conduct|Join and proceed |Account|
-      | https://membership.basketballengland.co.uk/NewFullAccount | edge    | Robert     | Lucas     | robert.lucaas.80@gmail.com | +46702064151 | female | Swedish     | Sweden           | Anna      | wife         | +46768778384 | 31 The Square, Tarland, Aberdeenshire Scotland | Aberchirder | AB34 4TX  | Aberdeenshire | Sweden    | EttEnkeltLösenord | EttEnkeltLösenord | prefer not to say | prefer not to say  |prefer not to say | prefer not to say    |                          |                 |       |
-      |                                                           | chrome  |            |           |                            |              | male   |             |                  |           |              |              |                                                |             |           |               |           |                   |                   |                   |                    |                  |                      |                          |                 |       |
-
+      | url                                                       | DateOfBirth | First Name | Last Name | Email                      | confirmEmail               |Mobilephone | gender | nationality | country of birth | full name | relationship | phonenumber | address line 1                                 | towncity   | post code | county        | country   | password          | retypepassword   | ethnicity         | sexual orientation | religion         | Account confirmation |Code of ethics and conduct|Join and proceed |Account|
+      | https://membership.basketballengland.co.uk/NewFullAccount | 12/02/1980  | Robert     | Lucas     | robert.lucaas.80@gmail.com | robert.lucaas.80@gmail.com |+46702064151|  male  |   Swedish   | Sweden           | Anna      | wife         | +46768778384| 31 The Square, Tarland, Aberdeenshire Scotland | Aberchirder| AB34 4TX  | Aberdeenshire | Sweden    | EttEnkeltLösenord | EttEnkeltLösenord| prefer not to say | prefer not to say  |prefer not to say | clicked              |clicked                   |clicked          |  yes  |
 
   #Scenario: Create new user last name is missing
     #Given User have correct url
