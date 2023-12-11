81168441de8d94360e85d38560c1c945b95ba5d1
RobertLucas <robert.lucaas.80@gmail.com>
2023-12-11 09:22:19 +0100

    Latest

--- src/test/java/stepDefinitions/MyStepdefs.java
+++ src/test/java/stepDefinitions/MyStepdefs.java
@@ -4,7 +4,6 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,6 +11,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
@@ -20,22 +20,8 @@ public class MyStepdefs<string, function> {
     private Registrator registrator;
 
     private WebDriver driver;
-    private WebDriver WebDriverRefrence;
-
-    @Given("User have correct url")
-    public void UserHaveCorrectUrl() {
-
-        //driver = new FirefoxDriver();
-        driver = new EdgeDriver();
-        //driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-
-        driver.get("https://membership.basketballengland.co.uk/NewFullAccount");
-        //driver.findElement(By.tagName("button")).click();
-
-        System.out.println("Correct url is entered");
-    }
+    private WebDriver WebDriverReference;
+    private String browser;
 
     @And("User fill in all member details")
     public void iFillInAllRequiredFields() throws Throwable {
@@ -160,10 +146,10 @@ public class MyStepdefs<string, function> {
     public void joinAndProceedAreClicked() throws InterruptedException {
 
         //Explicit wait, men får inte & funka :(
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         WebElement LastButton;
-        LastButton= wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#btnJoin")));
-        
+        LastButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input#btnJoin")));
+
         driver.findElement(By.cssSelector("input#btnJoin")).click();
         System.out.println("Join and proceed is clicked");
 
@@ -183,7 +169,6 @@ public class MyStepdefs<string, function> {
         search.sendKeys("12/02/1980");
 
         driver.findElement(By.id("member_firstname")).sendKeys("Robert");
-        //Thread.sleep(1000);
 
         WebElement A = driver.findElement(By.id("member_lastname"));
         String text = A.getAttribute("value");
@@ -192,11 +177,6 @@ public class MyStepdefs<string, function> {
             System.out.println("'Last Name' must be entered");
 
         }
-
-        }
-
-    @When("User sets password missmatch")
-    public void userSetsPasswordMissmatch() {
 
     }
 
@@ -216,20 +196,173 @@ public class MyStepdefs<string, function> {
     public void userSetsPasswordMismatch() {
     }
 
+    @When("User is on the registration page")
+    public void userIsOnTheRegistrationPage() {
+        String pageTitle = driver.getTitle();
+        Assert.assertEquals(pageTitle, "Basketball England Members Area", "Title is not matching");
+
+    }
+
+    @And("User enter {string} and {string}")
+    public void enterUserAnd(String firstName, String lastName) {
+
+        driver.findElement(By.id("member_firstname")).sendKeys(firstName);
+        driver.findElement(By.id("member_lastname")).sendKeys(lastName);
+    }
+
+    @And("User enter email {string} and {string}")
+    public void userEnterEmailAnd(String Email, String confirmEmail) {
+
+        driver.findElement(By.id("member_firstname")).sendKeys(Email);
+        driver.findElement(By.id("member_firstname")).sendKeys(confirmEmail);
+
+    }
+
+    @And("User fill in {string}")
+    public void userFillIn(String Mobilephone) {
+
+        driver.findElement(By.id("signup_memberdetails_mobilephone")).sendKeys("+46702064151");
+
+    }
+
+    @And("User selects {string}")
+    public void userSelects(String female, String male) {
+
+        Select dropDown = new Select(driver.findElement(By.id("signup_memberdetails_gender")));
+
+        dropDown.selectByIndex(2);
+
+    }
+
+    @And("User enters {string} and {string}")
+    public void userEntersAnd(String nationality, String countryofbirth) {
+
+        Select dropDown = new Select(driver.findElement(By.id("sign_up_12")));
+        dropDown.selectByIndex(211);
+
+        dropDown = new Select(driver.findElement(By.id("sign_up_13")));
+        dropDown.selectByIndex(211);
+
+    }
+
+    @And("User enter emergency contact details {string} and {string} and {string}")
+    public void userEnterEmergencyContactDetailsAndAnd(String fullname, String relationship, String arg2, String phonenumber) {
+
+        driver.findElement(By.id("signup_nextofkin_name")).sendKeys("Anna Lucas");
+
+        driver.findElement(By.id("signup_nextofkin_relationship")).sendKeys("Wife");
+
+        driver.findElement(By.id("signup_nextofkin_contact")).sendKeys("+46768778384");
+
+        System.out.println("All emergency contact details are filled in");
+
+    }
+
+    @And("User fills in address {string} and {string} and {string} and {string} and {string}")
+    public void userFillsInAddressAndAndAndAnd(String addressline1, String towncity, String postcode, String country, String country2) {
+
+        driver.findElement(By.id("txtAddressLine1")).sendKeys("31 The Square, Tarland, Aberdeenshire Scotland");
+
+        driver.findElement(By.id("txtAddressTownCity")).sendKeys("Aberchirder");
+
+        driver.findElement(By.id("txtPostCode")).sendKeys("AB34 4TX");
+
+        Select dropDown = new Select(driver.findElement(By.id("ddlCounty")));
+        dropDown.selectByIndex(1);
+
+        dropDown = new Select(driver.findElement(By.name("CountryId")));
+        dropDown.selectByIndex(211);
+
+        System.out.println("Address is filled in");
+
+    }
+
+    @And("User sets {string} and {string}")
+    public void userSetsAnd(String password, String retypepassword) {
+
+        driver.findElement(By.id("sign_up_9")).sendKeys("EttEnkeltLösenord");
+
+        driver.findElement(By.id("sign_up_10")).sendKeys("EttEnkeltLösenord");
+
+        System.out.println("Password is set");
+
+    }
+
+    @And("User fills in all anonymous information {string} and {string} and {string}")
+    public void userFillsInAllAnonymousInformationAndAnd(String sexualorientation, String religion) {
+
+        Select dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_ethnicity")));
+        dropDown.selectByIndex(1);
+
+        dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_sexualorientation")));
+        dropDown.selectByIndex(1);
+
+        dropDown = new Select(driver.findElement(By.id("signup_anonymousinfo_religion")));
+        dropDown.selectByIndex(1);
+
+        System.out.println("All anonymous information i set");
+
+    }
+
+    @And("{string} is checked")
+    public void isChecked(String Accountconfirmation) {
+
+        driver.findElement(By.cssSelector("label[for=sign_up_25")).click();
+
+        driver.findElement(By.cssSelector("label[for=sign_up_26")).click();
+
+        System.out.println("Account confirmation i checked");
+
+    }
+
+    @Given("User has the correct {string}")
+    public void userHasTheCorrect(String url) {
+
+            driver = new EdgeDriver();
+            driver.manage().window().maximize();
+            driver.navigate().to("https://membership.basketballengland.co.uk/NewFullAccount");
+            driver.get(url);
+            driver.get("https://membership.basketballengland.co.uk/NewFullAccount");
+            driver.get("https://membership.basketballengland.co.uk/NewFullAccount");
+
+            System.out.println("Correct url is entered");
+        }
+
+    }
+
+
     class Registrator {
 
-            @Test
-
-            public string RegisterUser() throws InterruptedException {
-
-                return getResult();
-            }
-
-            public string getResult() {
-                return registrator.getResult();
-            }
+        private string result;
 
 
-            }
+        public string getResult() {
+            return result;
         }
+
+        private class string {
+        }
+
+
+    /*@Given("User is using {string} as {string}")
+    public void userIsUsingAs(String edge, String chrome) {
+
+            {
+                if (browser.equals(("edge"))) {
+                    driver = new EdgeDriver();
+                } else if (browser.equals("chrome")) {
+                    driver = new ChromeDriver();
+                }
+
+                driver.get("https://membership.basketballengland.co.uk/NewFullAccount");
+
+            }*/
+
+
+    }
+
+
+
+
+
 
--- src/test/resources/features/Registrator.feature
+++ src/test/resources/features/Registrator.feature
@@ -1,18 +1,27 @@
 Feature: Create New User
 
-  Scenario Outline: Registrate a new user on webpage
-    Given User have correct <url>
-    And User fill in all member details
-    And User fill in all emergency contact details
-    And User fill in address
-    And User sets password
-    And User fills in all anonymous information
-    And Account confirmation is checked
+  Scenario Outline: Registration a new user on webpage
+    Given User has the correct "<url>"
+    #Given User is using "edge" as "<browser>"
+    When User is on the registration page
+    And User enter "<First Name>" and "<Last Name>"
+    And User enter email "<Email>" and "<email>"
+    And User fill in "<Mobile phone>"
+    And User selects "<gender>"
+    And User enters "<nationality>" and "<country of birth>"
+    And User enter emergency contact details "<full name>" and "<relationship>" and "<phone number>"
+    And User fills in address "<address line 1>" and "<town/city>" and "<post code>" and "<country>" and "<country 2>"
+    And User sets "<password>" and "<retype password>"
+    And User fills in all anonymous information "<ethnicity>" and "<sexual orientation>" and "<religion>"
+    And "<Account confirmation>" is checked
     And Code of ethics and conduct are checked
     And Join and proceed are clicked
     Then An account is created
     Examples:
-      |  |  |  |
+      | url                                                       | browser | First Name | Last Name | Email                      | Mobile phone | gender | nationality | country of birth | full name | relationship | phone number | address line 1                                 | town/city   | post code | country       | country 2 | password          | retype password   | ethnicity | sexual orientation | religion | Account confirmation |  |
+      | https://membership.basketballengland.co.uk/NewFullAccount | edge    | Robert     | Lucas     | robert.lucaas.80@gmail.com | +46702064151 | female | Swedish     | Sweden           | Anna      | wife         | +46768778384 | 31 The Square, Tarland, Aberdeenshire Scotland | Aberchirder | AB34 4TX  | Aberdeenshire | Sweden    | EttEnkeltLösenord | EttEnkeltLösenord |           |                    |          |                      |  |
+      |                                                           | chrome  |            |           |                            |              | male   |             |                  |           |              |              |                                                |             |           |               |           |                   |                   |           |                    |          |                      |  |
+      |                                                           |         |            |           |                            |              |        |             |                  |           |              |              |                                                |             |           |               |           |                   |                   |           |                    |          |                      |  |
 
   Scenario: Create new user last name is missing
     Given User have correct url
@@ -39,8 +48,6 @@ Feature: Create New User
     Then An account is NOT created
 
   #Verifiering skall ske på varje scenario med minst en Junit assert
-
-  #Testfallen skall skapas genom en och enbart en Scenario Outline ifrån en feature-fil
 
   #Testfallen skall köras på minst två browsrar
 
